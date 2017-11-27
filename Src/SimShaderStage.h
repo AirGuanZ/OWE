8a72b1c44b2b412abec91fd14534505547537ae9
AirGuanZ <AirGuanZ@gmail.com>
2017-11-27 21:46:43 +0800

    重新设计ShaderStage

--- Src/SimShaderStage.h
+++ Src/SimShaderStage.h
@@ -1,184 +1,92 @@
 /*================================================================
 Filename: SimShaderStage.h
-Date: 2017.11.25
+Date: 2017.11.26
 Created by AirGuanZ
 ================================================================*/
 #ifndef __SIMSHADER_STAGE_H__
 #define __SIMSHADER_STAGE_H__
 
 #include <cassert>
+#include <d3d11.h>
 
 #include "SimShaderObjectBinding.h"
 #include "SimShaderReflection.h"
+#include "SimShaderUncopiable.h"
 
 #include "SimShaderConstantBuffer.h"
-#include "SimShaderResource.h"
 #include "SimShaderSampler.h"
+#include "SimShaderResource.h"
 
 namespace _SimShaderAux
 {
-    template<ShaderStageSelector StageSelector>
-    class _ShaderStage;
-
-    template<ShaderStageSelector StageSelector>
-    class _ShaderSpec;
+    template<ShaderStageSelector>
+    class _ShaderStageSpec;
 
     template<>
-    class _ShaderSpec<SS_VS>
+    class _ShaderStageSpec<SS_VS>
     {
     public:
         using D3DShaderType = ID3D11VertexShader;
 
-        static D3DShaderType *InitShader(ID3D11Device *dev, void *shaderCompiled, UINT length)
+        static D3DShaderType *InitShader(ID3D11Device *dev, void *shaderCompiled, SIZE_T length)
         {
-            assert(dev);
-            assert(shaderCompiled && length);
-
+            assert(dev != nullptr);
+            assert(shaderCompiled != nullptr && length > 0);
             D3DShaderType *rt = nullptr;
             if(FAILED(dev->CreateVertexShader(shaderCompiled, length, nullptr, &rt)))
-                throw SimShaderError("Failed to create vertex shader");
+                return nullptr;
             return rt;
         }
 
         static void BindShader(ID3D11DeviceContext *DC, D3DShaderType *shader)
         {
-            assert(DC);
+            assert(DC != nullptr);
             DC->VSSetShader(shader, nullptr, 0);
         }
     };
 
     template<>
-    class _ShaderSpec<SS_PS>
+    class _ShaderStageSpec<SS_PS>
     {
     public:
         using D3DShaderType = ID3D11PixelShader;
 
-        static D3DShaderType *InitShader(ID3D11Device *dev, void *shaderCompiled, UINT length)
+        static D3DShaderType *InitShader(ID3D11Device *dev, void *shaderCompiled, SIZE_T length)
         {
-            assert(dev);
-            assert(shaderCompiled && length);
-
+            assert(dev != nullptr);
+            assert(shaderCompiled != nullptr && length > 0);
             D3DShaderType *rt = nullptr;
             if(FAILED(dev->CreatePixelShader(shaderCompiled, length, nullptr, &rt)))
-                throw SimShaderError("Failed to create pixel shader");
+                return nullptr;
             return rt;
         }
 
         static void BindShader(ID3D11DeviceContext *DC, D3DShaderType *shader)
         {
-            assert(DC);
+            assert(DC != nullptr);
             DC->PSSetShader(shader, nullptr, 0);
         }
-    };
+    }; 
 
     template<ShaderStageSelector StageSelector>
-    class _NormalShaderStage
+    class _ShaderStage : public _Uncopiable
     {
     public:
-        using ShaderSpec = _ShaderSpec<StageSelector>;
+        using StageSpec = _ShaderStageSpec<StageSelector>;
+        using D3DShaderType = typename StageSpec::D3DShaderType;
 
-        _NormalShaderStage(ID3D11Device *dev, void *shaderCompiled, UINT length)
-        {
-            shader_ = ShaderSpec::InitShader(dev, shaderCompiled, length);
-            ID3D11ShaderReflection *ref = _GetShaderReflection(shaderCompiled, length);
+        _ShaderStage(ID3D11Device *dev, void shaderByteCode, SIZE_T length);
+        ~_ShaderStage(void);
 
-            std::map<std::string, _CBInfo> cbInfos;
-            _GetConstantBuffers(ref, &cbInfos);
-            for(auto it : cbInfos)
-                cbMgr_.AddBuffer(it.first, it.second.slot, it.second.byteSize);
+        void BindShader(ID3D11DeviceContext *DC);
+        void UnbindShader(ID3D11DeviceContext *DC);
 
-            std::map<std::string, UINT> texInfos;
-            _GetShaderTextures(ref, &texInfos);
-            for(auto it : texInfos)
-                srMgr_.AddShaderResource(it.first, it.second);
-
-            std::map<std::string, UINT> samInfos;
-            _GetShaderSamplers(ref, &samInfos);
-            for(auto it : samInfos)
-                samMgr_.AddShaderSampler(it.first, it.second);
-
-            ref->Release();
-        }
-        
-        void BindShader(ID3D11DeviceContext *DC)
-        {
-            assert(DC);
-            ShaderSpec::BindShader(DC, shader_);
-        }
-
-        void BindConstantBuffers(ID3D11DeviceContext *DC)
-        {
-            assert(DC);
-            cbMgr_.Bind(DC);
-        }
-
-        void BindShaderResources(ID3D11DeviceContext *DC)
-        {
-            assert(DC);
-            srMgr_.Bind(DC);
-        }
-
-        void BindShaderSamplers(ID3D11DeviceContext *DC)
-        {
-            assert(DC);
-            samMgr_.Bind(DC);
-        }
-
-        void UnbindConstantBuffers(ID3D11DeviceContext *DC)
-        {
-            assert(DC);
-            cbMgr_.Unbind(DC);
-        }
-
-        void UnbindShaderResources(ID3D11DeviceContext *DC)
-        {
-            assert(DC);
-            srMgr_.Unbind(DC);
-        }
-
-        void UnbindShaderSamplers(ID3D11DeviceContext *DC)
-        {
-            assert(DC);
-            samMgr_.Unbind(DC);
-        }
-
-        void BindAllResources(ID3D11DeviceContext *DC)
-        {
-            BindConstantBuffers(DC);
-            BindShaderResources(DC);
-            BindShaderSamplers(DC);
-        }
-
-        void UnbindAllResources(ID3D11DeviceContext *DC)
-        {
-            UnbindConstantBuffers(DC);
-            UnbindShaderResources(DC);
-            UnbindShaderSamplers(DC);
-        }
-        
-        template<typename _BufferType, bool _IsDynamic = true>
-        _ConstantBufferObject<_BufferType, StageSelector, _IsDynamic> *
-        GetConstantBufferObject(ID3D11Device *dev, const std::string &name, const _BufferType *initData = nullptr)
-        {
-            return cbMgr_.GetConstantBuffer<_BufferType, _IsDynamic>(dev, name, initData);
-        }
-
-        _ShaderResourceObject<StageSelector> *GetShaderResourceObject(const std::string &name)
-        {
-            return srMgr_.GetShaderResourceObject(name);
-        }
-
-        _ShaderSamplerObject<StageSelector> *GetShaderSamplerObject(const std::string &name)
-        {
-            return samMgr_.GetShaderSamplerObject(name);
-        }
+        _ConstantBufferManager<StageSelector> *CreateConstantBufferManager(void);
+        _ShaderResourceManager<StageSelector> *CreateShaderResourceManager(void);
+        _ShaderSamplerManager<StageSelector> *CreateShaderSamplerManager(void);
 
     private:
-        _ConstantBufferManager<StageSelector> cbMgr_;
-        _ShaderResourceManager<StageSelector> srMgr_;
-        _ShaderSamplerManager<StageSelector> samMgr_;
-
-        typename ShaderSpec::D3DShaderType *shader_;
+        D3DShaderType *shader_;
     };
 }
 
